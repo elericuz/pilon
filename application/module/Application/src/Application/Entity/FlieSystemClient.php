493ad44bf3228412b51ba78c0c882858dcba10cb
Eric Valera Miller <eric@pragmum.com>
2015-03-23 23:32:25 -0500

    BAEN-003 now it's possible to add or create files/folders by clients.

---
+++ application/module/Application/src/Application/Entity/FlieSystemClient.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace Application\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FlieSystemClient
+ *
+ * @ORM\Table(name="flie_system_client", indexes={@ORM\Index(name="has_client", columns={"clii_id"}), @ORM\Index(name="has_fs", columns={"fisi_id"})})
+ * @ORM\Entity(repositoryClass="Application\Entity\Repository\FileSystemClientRepository")
+ */
+class FlieSystemClient
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="fsci_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $fsciId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="fsci_parent_id", type="integer", nullable=true)
+     */
+    private $fsciParentId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fscv_real_name", type="string", length=255, nullable=true)
+     */
+    private $fscvRealName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fsct_description", type="text", nullable=true)
+     */
+    private $fsctDescription;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fscd_upload_date", type="datetime", nullable=true)
+     */
+    private $fscdUploadDate;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="fsci_status", type="integer", nullable=true)
+     */
+    private $fsciStatus = '1';
+
+    /**
+     * @var \Application\Entity\Client
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Entity\Client")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="clii_id", referencedColumnName="clii_id")
+     * })
+     */
+    private $clii;
+
+    /**
+     * @var \Application\Entity\FileSystem
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Entity\FileSystem")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="fisi_id", referencedColumnName="fisi_id")
+     * })
+     */
+    private $fisi;
+
+
+
+    /**
+     * Get fsciId
+     *
+     * @return integer 
+     */
+    public function getFsciId()
+    {
+        return $this->fsciId;
+    }
+
+    /**
+     * Set fsciParentId
+     *
+     * @param integer $fsciParentId
+     * @return FlieSystemClient
+     */
+    public function setFsciParentId($fsciParentId)
+    {
+        $this->fsciParentId = $fsciParentId;
+
+        return $this;
+    }
+
+    /**
+     * Get fsciParentId
+     *
+     * @return integer 
+     */
+    public function getFsciParentId()
+    {
+        return $this->fsciParentId;
+    }
+
+    /**
+     * Set fscvRealName
+     *
+     * @param string $fscvRealName
+     * @return FlieSystemClient
+     */
+    public function setFscvRealName($fscvRealName)
+    {
+        $this->fscvRealName = $fscvRealName;
+
+        return $this;
+    }
+
+    /**
+     * Get fscvRealName
+     *
+     * @return string 
+     */
+    public function getFscvRealName()
+    {
+        return $this->fscvRealName;
+    }
+
+    /**
+     * Set fsctDescription
+     *
+     * @param string $fsctDescription
+     * @return FlieSystemClient
+     */
+    public function setFsctDescription($fsctDescription)
+    {
+        $this->fsctDescription = $fsctDescription;
+
+        return $this;
+    }
+
+    /**
+     * Get fsctDescription
+     *
+     * @return string 
+     */
+    public function getFsctDescription()
+    {
+        return $this->fsctDescription;
+    }
+
+    /**
+     * Set fscdUploadDate
+     *
+     * @param \DateTime $fscdUploadDate
+     * @return FlieSystemClient
+     */
+    public function setFscdUploadDate($fscdUploadDate)
+    {
+        $this->fscdUploadDate = $fscdUploadDate;
+
+        return $this;
+    }
+
+    /**
+     * Get fscdUploadDate
+     *
+     * @return \DateTime 
+     */
+    public function getFscdUploadDate()
+    {
+        return $this->fscdUploadDate;
+    }
+
+    /**
+     * Set fsciStatus
+     *
+     * @param integer $fsciStatus
+     * @return FlieSystemClient
+     */
+    public function setFsciStatus($fsciStatus)
+    {
+        $this->fsciStatus = $fsciStatus;
+
+        return $this;
+    }
+
+    /**
+     * Get fsciStatus
+     *
+     * @return integer 
+     */
+    public function getFsciStatus()
+    {
+        return $this->fsciStatus;
+    }
+
+    /**
+     * Set clii
+     *
+     * @param \Application\Entity\Client $clii
+     * @return FlieSystemClient
+     */
+    public function setClii(\Application\Entity\Client $clii = null)
+    {
+        $this->clii = $clii;
+
+        return $this;
+    }
+
+    /**
+     * Get clii
+     *
+     * @return \Application\Entity\Client 
+     */
+    public function getClii()
+    {
+        return $this->clii;
+    }
+
+    /**
+     * Set fisi
+     *
+     * @param \Application\Entity\FileSystem $fisi
+     * @return FlieSystemClient
+     */
+    public function setFisi(\Application\Entity\FileSystem $fisi = null)
+    {
+        $this->fisi = $fisi;
+
+        return $this;
+    }
+
+    /**
+     * Get fisi
+     *
+     * @return \Application\Entity\FileSystem 
+     */
+    public function getFisi()
+    {
+        return $this->fisi;
+    }
+}
---
+++ application/module/Repository/src/Repository/Controller/FolderController.php
@@ -0,0 +1,48 @@
+<?php
+namespace Repository\Controller;
+
+use Zend\View\Model\ViewModel;
+use Common\Controller\MainController;
+use Application\Entity\FileSystem;
+use Application\Entity\FlieSystemClient;
+
+/**
+ * FolderController
+ *
+ * @author Eric Valera Miller
+ *
+ * @version 1.0
+ *
+ */
+class FolderController extends MainController
+{
+    public function createAction()
+    {
+        $request = $this->getRequest();
+        if($request->isPost())
+        {
+            $Client_obj = $this->em->find('Application\Entity\Client', 1);
+
+            $FS_obj = new FileSystem();
+            $FS_obj->setFisiParentId(0);
+            $FS_obj->setFisiType(0);
+            $FS_obj->setFisvName(md5($request->getPost('folder_name')));
+            $FS_obj->setFisvRealName(trim($request->getPost('folder_name')));
+            $FS_obj->setFisdUploadDate(new \DateTime("now"));
+            $FS_obj->setFisvUploadIp($_SERVER['REMOTE_ADDR']);
+            $this->em->persist($FS_obj);
+
+            $FSC_obj = new FlieSystemClient();
+            $FSC_obj->setFsciParentId(0)
+                    ->setClii($Client_obj)
+                    ->setFisi($FS_obj)
+                    ->setFscvRealName(trim($request->getPost('folder_name')))
+                    ->setFscdUploadDate(new \DateTime("now"));
+            $this->em->persist($FSC_obj);
+
+            $this->em->flush();
+        }
+
+        return $this->redirect()->toRoute('my-repo');
+    }
+}
